01165203384bb739523e85294d7e8998908958a9
Caleb001-tech <caleboffiong101@gmail.com>
2026-07-02 13:41:08 +0100

    Add files via upload
    This is the first file for number one of the assignment.

---
+++ CPE121 Assignment 1..cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "JAMES, CALEB OFFIONG" << endl;
+	
+	cout << "25/EG/CO/001" << endl;
+	
+	cout << "COMPUTER ENGINEERING" << endl;
+	
+	return 0;
+}
