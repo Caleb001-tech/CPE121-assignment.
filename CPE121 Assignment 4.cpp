8432b94d1161102bfe6afdafe24923bba1e1a63f
Caleb001-tech <caleboffiong101@gmail.com>
2026-07-02 14:23:07 +0100

    Add files via upload
    This is the file for CPE121 assignment number 4.

---
+++ CPE121 Assignment 4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// This cout message prints a welcome message to the screen
+	cout << "WELCOME EVERYONE TO MY CPE121 ASSIGNMENT WORKSPACE!!!" << endl;
+	
+	// This cout message print out my favourite food
+	cout << "MY FAVOURITE FOOD IS RICE AND STEW WITH GOAT MEAT" << endl;
+	
+	// This cout message prints out my worst course
+	cout << "MY WORST COURSE IS PNE567" << endl;
+	
+	// This cout message prints out my favourite programming language
+	cout << "MY FAVOURITE PROGRAMMING LANGUAGE IS PYTHON" << endl;
+	
+	return 0;
+	
+}
